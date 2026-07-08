6c480198863a74d2a338c5cb3701044cdb7c2394
Theophilus-git <theophilusofori819@gmail.com>
2026-07-08 01:43:59 +0000

    Builded the data models

--- invProject/invApp/models.py
+++ invProject/invApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      dels.AutoField(primary_key=True)
+    name = models.CharField(max=100)
+    sku = models.CharField(max=100, unique=True) # Stock keeping unit
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    Supplier = models.CharField(max=100)
+
+    def __str__(self):
+        return self.name
