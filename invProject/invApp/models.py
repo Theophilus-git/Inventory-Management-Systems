99a91b573ccfeaf7c516037e2af1b2fd22cef428
Theophilus-git <theophilusofori819@gmail.com>
2026-07-09 23:57:57 +0000

    Add forms.py and link ProductForm to Product model

--- invProject/invApp/models.py
+++ invProject/invApp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    product_id = models.AutoField(primary_key=True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      dels.AutoField(primary_key=True)
-    name = models.CharField(max=100)
-    sku = models.CharField(max=100, unique=True) # Stock keeping unit
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    sku = models.CharField(max_length=100, unique=True) # Stock keeping unit
     price = models.FloatField()
     quantity = models.IntegerField()
-    Supplier = models.CharField(max=100)
+    supplier = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
